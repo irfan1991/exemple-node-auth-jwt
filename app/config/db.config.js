cf46b18598d425e68d47cc52f432994840808b5d
irfan1991 <irfanprasetyo91@gmail.com>
2020-06-24 20:24:46 +0700

    ubah config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "irfan",
-    PASSWORD: "irfan",
-    DB: "testdb2",
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "b9ad173eb97efd",
+    PASSWORD: "2529ec80",
+    DB: "heroku_b85dba1e77c7a7b",
     dialect: "mysql",
     pool: {
       max: 105,
@@ -12,3 +12,5 @@ module.exports = {
     }
   };
   
+
+// CLEARDB_DATABASE_URL: mysql://b9ad173eb97efd:2529ec80@us-cdbr-east-05.cleardb.net/heroku_b85dba1e77c7a7b?reconnect=true
