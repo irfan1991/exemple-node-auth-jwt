e1c66cf13e254bc0999e7e4c3f6272ec6a1cc587
irfan1991 <irfanprasetyo91@gmail.com>
2020-06-25 11:37:59 +0700

    change configuration db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "sql102.epizy.com",
-    USER: "epiz_26099117",
-    PASSWORD: "RI9wbrmfgaOz",
-    DB: "epiz_26099117_articles",
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "b9ad173eb97efd",
+    PASSWORD: "2529ec80",
+    DB: "heroku_b85dba1e77c7a7b",
     dialect: "mysql",
     pool: {
       max: 105,
@@ -13,18 +13,5 @@ module.exports = {
   };
   
 
-  // module.exports = {
-  //   HOST: "us-cdbr-east-05.cleardb.net",
-  //   USER: "b9ad173eb97efd",
-  //   PASSWORD: "2529ec80",
-  //   DB: "heroku_b85dba1e77c7a7b",
-  //   dialect: "mysql",
-  //   pool: {
-  //     max: 105,
-  //     min: 0,
-  //     acquire: 30000,
-  //     idle: 10000
-  //   }
-  // };
   
 // CLEARDB_DATABASE_URL: mysql://b9ad173eb97efd:2529ec80@us-cdbr-east-05.cleardb.net/heroku_b85dba1e77c7a7b?reconnect=true
